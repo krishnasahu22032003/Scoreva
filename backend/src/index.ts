37c3d1f5a7804c383e0ef6c8c0a968b394dea956
krishna sahu <krishna.sahu.work@gmail.com>
2026-02-18 01:28:51 +0530

    Basic backend setup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world")
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("ws project")
+})
+
+
+app.listen(3000 , ()=>{
+    console.log("App running on port 3000")
+})
