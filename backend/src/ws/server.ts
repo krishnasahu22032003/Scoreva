3c85ac69202c5aee7c6bfdecdd66a05f4f6575db
krishna sahu <krishna.sahu.work@gmail.com>
2026-02-22 01:42:49 +0530

    Arcjet function added in attachwebsocketserver security is working

--- backend/src/ws/server.ts
+++ backend/src/ws/server.ts
@@ -1,6 +1,8 @@
 import WebSocket, { WebSocketServer } from "ws";
 import { Server } from "http";
 import type { Match } from "../../generated/prisma/index.js";
+import { wsArcjet } from "../arcjet.js";
+import type { ArcjetNodeRequest } from "@arcjet/node";
 
 export interface ExtendedWebSocket extends WebSocket{
 
@@ -28,9 +30,26 @@ export function broadcast(wss: WebSocketServer, payload: unknown) {
 
 export function attachWebSocketServer(server: Server) {
 
+    
     const wss = new WebSocketServer({ server, path: '/ws' , maxPayload:1024 * 1024  });
 
-    wss.on("connection",(socket:ExtendedWebSocket)=>{
+    wss.on("connection",async (socket:ExtendedWebSocket ,req:ArcjetNodeRequest)=>{
+        if (wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req , {requested:1});
+
+                if (decision.isDenied()) {
+                  const code = decision.reason.isRateLimit() ? 1013 :1008 ;
+                  const reason = decision.reason.isRateLimit() ? "rate limit exceeded" : "access denied" ;
+                  socket.close(code , reason);
+                  return ;
+                }
+            } catch (e) {
+                console.error('WS upgrade protection error', e);
+                socket.close(1011,"Server Security error");
+                return;
+            }
+        }
         socket.isAlive = true;
         socket.on("pong",()=>{socket.isAlive = true });
         sendJson(socket,{type:'welcome'});
