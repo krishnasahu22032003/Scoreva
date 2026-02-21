30fe2103123a02b568516be6ef816a213e63b4ad
krishna sahu <krishna.sahu.work@gmail.com>
2026-02-21 23:51:04 +0530

    Ping pong logic for websocket server done

--- backend/src/ws/server.ts
+++ backend/src/ws/server.ts
@@ -2,6 +2,13 @@ import WebSocket, { WebSocketServer } from "ws";
 import { Server } from "http";
 import type { Match } from "../../generated/prisma/index.js";
 
+export interface ExtendedWebSocket extends WebSocket{
+
+isAlive:Boolean
+
+}
+
+
 export function sendJson(socket: WebSocket, payload: unknown) {
 
     if (socket.readyState !== WebSocket.OPEN) return;
@@ -13,20 +20,38 @@ export function sendJson(socket: WebSocket, payload: unknown) {
 export function broadcast(wss: WebSocketServer, payload: unknown) {
 
     for (const client of wss.clients) {
-        if (client.readyState !== WebSocket.OPEN) return;
+        if (client.readyState !== WebSocket.OPEN) continue;
         client.send(JSON.stringify(payload))
     }
 
 }
 
 export function attachWebSocketServer(server: Server) {
+
     const wss = new WebSocketServer({ server, path: '/ws' , maxPayload:1024 * 1024  });
 
-    wss.on("connection",(socket:WebSocket)=>{
-
+    wss.on("connection",(socket:ExtendedWebSocket)=>{
+        socket.isAlive = true;
+        socket.on("pong",()=>{socket.isAlive = true });
         sendJson(socket,{type:'welcome'});
         socket.on("error",console.error);
     })
+
+const interval = setInterval(() => {
+
+    wss.clients.forEach((ws)=>{
+            
+    const socket = ws as ExtendedWebSocket;
+      if(socket.isAlive === false) return ws.terminate();
+    socket.isAlive = false;
+    socket.ping;
+
+    })
+  
+
+}, 30000);
+
+wss.on("close",()=>{clearInterval(interval)});
 function broadcastMatchCreated(match:Match){
 
 broadcast(wss,{type:'match_created',data:match})
