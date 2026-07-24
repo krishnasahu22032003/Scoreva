47e047611b09cb1710671cf48bb35bb67bfd220e
krishna sahu <krishna.sahu.work@gmail.com>
2026-07-24 22:46:51 +0530

    update code

--- backend/src/Routes/UserAuthRoutes.ts
+++ backend/src/Routes/UserAuthRoutes.ts
@@ -9,7 +9,7 @@ const UserRouter = express.Router();
 UserRouter.post("/signup",UserSignUp);
 UserRouter.post("/signin",UserSignIn);
 UserRouter.post("/admin/signup",AdminSignUp)
-UserRouter.post("/signout",AuthMiddleware,UserSignOut);
+UserRouter.post("/signout",UserSignOut);
 UserRouter.get("/user-details",AuthMiddleware,GetUserDetails);
 
 export default UserRouter;
